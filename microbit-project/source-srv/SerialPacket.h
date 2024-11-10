cd593b49bb7d178c50f34bd7e89c4735b80fb140
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2024-11-10 16:06:46 +0100

    modif radiopacket

--- microbit-project/source-srv/SerialPacket.h
+++ microbit-project/source-srv/SerialPacket.h
@@ -1,7 +1,11 @@
 #pragma once
+#include "MicroBit.h"
 
 class SerialPacket{
     public:
         SerialPacket();
         ~SerialPacket();
+
+    private:
+        uint8_t *buffer;
 };
--- microbit-project/source-srv/main.cpp
+++ microbit-project/source-srv/main.cpp
@@ -1,10 +1,23 @@
-#include "MicroBit.h"
-MicroBit    uBit;
+#include "RadioServer.h"
 
+MicroBit    uBit;
+RadioServer rserv(&uBit, 65534);
+SerialServer sserv(&uBit);
+
+void onRadioDataReceive(MicroBitEvent){
+    rserv.receivePacket();
+}
+
+void onSerialDataReceive(MicroBitEvent){
+    sserv.receiveData();
+}
 
 int main()
 {
     uBit.init();
-    uBit.display.scroll(uBit.getSerial());
-
+    rserv.init(&sserv);
+    sserv.init(&rserv);
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onRadioDataReceive);
+    uBit.messageBus.listen(MICROBIT_ID_SERIAL, MICROBIT_SERIAL_EVT_DELIM_MATCH, onSerialDataReceive);
+    rserv.run();
 }
